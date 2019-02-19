d4d8ce3018d34623a4d9b7acb2566777458cad70
Aman Jain <aj161198@gmail.com>
2019-02-19 20:09:47 +0530

    Deleted dataset and others

--- histogram_based_checking.py
+++ histogram_based_checking.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import os
+import matplotlib.pyplot as plt
 
 csv_string = ""
 
@@ -36,28 +37,29 @@ def get_roi(path, filename):
     return roi, key, bboxs[0]
 
 
-def solve(img, key, bbox, filename):
+def solve(img, key, bbox):
     val = 0
     reduction = -1
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("blur", gray)
+    cv2.imshow("gray", gray)
+
+    gray = cv2.GaussianBlur(gray, (5,5), 0)
 
     ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
-    #cv2.imshow("mask", mask)
+    cv2.imshow("mask", mask)
 
     dx = cv2.Scharr(gray, cv2.CV_64F, 1, 0)
     dx = cv2.convertScaleAbs(dx)
-    #cv2.imshow("dx", dx)
+    cv2.imshow("dx", dx)
 
     dx = cv2.bitwise_and(dx, mask)
-
+    cv2.imshow(" ", dx)
     h, w = dx.shape
     bins = np.sum(dx, 1)
     smooth = gaussian_filter(bins, sigma=5)
 
-    index = np.argmax(smooth) + 1
     mean = np.sum(smooth) / len(bins)
-    # print(mean)
+    print(mean)
 
     drawing = np.zeros(dx.shape, np.uint8)
     indices = np.where(smooth > mean)
@@ -65,21 +67,21 @@ def solve(img, key, bbox, filename):
         drawing[index:index + 1, :] = 255
 
 
-    #cv2.imshow("drawing", drawing)
+    cv2.imshow("drawing", drawing)
     # print([key[1] - bbox[1]], [key[0] - bbox[0]])
     # print(drawing[key[1] - bbox[1]][key[0] - bbox[0]])
     if drawing[key[1] - bbox[1]][key[0] - bbox[0]] == 255:
         val = 1
-        reduction = cv2.countNonZero(drawing) * 1.0 / (h * w) * 100
+        reduction = 100 - cv2.countNonZero(drawing) * 1.0 / (h * w) * 100
 
-    # else:
-    #     print(filename)
+    else:
+    	print(filename)
 
-
+    # cv2.waitKey(0)
     # plt.plot(smooth, range(h))
     # plt.show()
     #
-    # cv2.waitKey(0)
+
 
     return val, reduction
 
@@ -90,7 +92,7 @@ def background():
     while (1):
         ret, frame = cap.read()
         fgmask = fgbg.apply(frame)
-        ###cv2.imshow('frame', fgmask)
+        #cv2.imshow('frame', fgmask)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
@@ -99,6 +101,7 @@ def background():
 
 path = "/home/aman/number_plate_detector/dataset"
 filenames = os.listdir(path)
+# filenames = ['/home/aman/number_plate_detector/dataset/vlcsnap-2019-02-18-01h04m09s067.png']
 count = 0
 
 for filename in filenames:
@@ -108,13 +111,13 @@ for filename in filenames:
         csv_string = csv_string + filename + ","
 
         filename = os.path.join(path, filename)
-        # print(filename)
+        print(filename)
         img = cv2.imread(filename + ".png")
         roi, key, bbox = get_roi(path, filename)
         cv2.namedWindow("img", cv2.WINDOW_NORMAL)
 
-        #cv2.imshow("img", img)
-        val, reduction = solve(roi, key, bbox, filename)
+        cv2.imshow("img", img)
+        val, reduction = solve(roi, key, bbox)
 
         if val == 1:
             count = count + 1
@@ -124,4 +127,5 @@ for filename in filenames:
 print (count)
 with open('output.csv', 'w') as f:
     f.write(csv_string)
+
 
